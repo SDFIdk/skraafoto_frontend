1bec68eb1186b416252f405a67e066a635d1d755
Frank R Thomsen <frtho@sdfe.dk>
2022-05-25 14:59:37 +0200

    try to import saul utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import {SkraaFotoViewport} from './components/viewport.js'
 import {SkraaFotoMap} from './components/map.js'
 import {SkraaFotoSlider} from './components/slider.js'
-import {httpGet} from './modules/api.js'
 
-console.log(httpGet(''))
+// TODO: Pull from npm package instead
+import {get} from '../../saul/index.js'
+
+get({
+  token: '47dada7edade95277d7d0935ab20a593',
+  baseUrl: 'https://api.dataforsyningen.dk/skraafotoapi_test/'
+}, '/conformance')
+.then((response) => {
+  console.log('got response', response)
+})
