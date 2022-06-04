fa48258815dfb312a88e9103e2424b5046f5330e
Frank R Thomsen <frtho@sdfe.dk>
2022-06-04 19:55:18 +0200

    simplify config setup

--- esbuild.js
+++ esbuild.js
@@ -1,40 +1,38 @@
-require('dotenv').config({path: `../.env.${process.env.NODE_ENV}`})
-
-console.log(process.env)
-
-let build_config
 if (process.env.NODE_ENV === 'development') {
   // Development mode watches for file changes and rebuilds
-  build_config = {
+  require('esbuild').build({
+    entryPoints: {
+      skraafoto: 'src/index.js', 
+      skraafotostyle: 'src/index.css'
+    },
+    outdir: 'dist',
+    bundle: true,
     watch: {
       onRebuild(error, result) {
         if (error) console.error('watch build failed:', error)
         else console.log('watch build succeeded:', result)
       },
     }
-  }
+  })
+  .then((response) => {
+    console.log('watching...')
+  })
+  .catch(() => process.exit(1))
+
 } else {
   // Production build
-  build_config = {
+  require('esbuild').build({
+    entryPoints: {
+      skraafoto: 'src/index.js', 
+      skraafotostyle: 'src/index.css'
+    },
+    outdir: 'dist',
+    bundle: true,
     minify: true,
     sourcemap:true
-  }
-}
-
-require('esbuild').build({
-  entryPoints: {
-    skraafoto: 'src/index.js', 
-    skraafotostyle: 'src/index.css'
-  },
-  outdir: 'dist',
-  bundle: true,
-  ...build_config
-})
-.then((response) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('watching...')
-  } else {
+  })
+  .then((response) => {
     console.log('build finished')
-  }
-})
-.catch(() => process.exit(1))
+  })
+  .catch(() => process.exit(1))
+}
