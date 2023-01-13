756de9da877eaa50d974d5bd80fc6dc901ff14f6
Frank Thomsen <frtho@sdfi.dk>
2023-01-13 13:42:26 +0100

    finally a test what works

--- test/navigate.spec.js
+++ test/navigate.spec.js
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test'
 
+// Run tests in this file with portrait-like viewport.
+test.use({ headless: false, viewport: { width: 600, height: 900 } });
 
 test('Search for an address from start page', async ({ page }) => {
 
   await page.goto('/')
+
   await page.getByPlaceholder('Søg adresse eller stednavn').fill('Rentemestervej 8')
-  await page.getByPlaceholder('Søg adresse eller stednavn').press('Enter')
+  await page.getByText('Rentemestervej 8, 2400 København NV').click()
 
   // Expect main viewport to contain data
-  await expect(page.locator('#viewport-main')).toContainText("Billede af området omkring koordinat 721760 øst,6178876 nord set fra nord.", { timeout: 20000 })
+  await expect(page.locator('#viewport-main')).toContainText("Billede af området omkring koordinat 721760 øst,6178876 nord set fra nord.", { timeout: 10000 })
 })
