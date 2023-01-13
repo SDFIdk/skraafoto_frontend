ee1921f56ad7dbc6701aac21c318a6f79303f7dd
Frank Thomsen <frtho@sdfi.dk>
2023-01-13 10:29:38 +0100

    add playwright test framework

---
+++ test/navigate.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test'
+
+test('Search for an address from start page', async ({ page }) => {
+  await page.goto('http://localhost:8000/')
+  await page.getByPlaceholder('Søg adresse eller stednavn').fill('Rentemestervej 8')
+  await page.getByPlaceholder('Søg adresse eller stednavn').press('Enter')
+
+  // Expect main viewport to contain data
+  await expect(page.locator('#viewport-main')).toContainText("Billede af området omkring koordinat 721760 øst,6178876 nord set fra nord.", { timeout: 15000 })
+})
