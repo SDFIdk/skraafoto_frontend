60f158dbbfc43fc2f6bc8491d7f93d1094d6aabf
Frank Thomsen <frtho@sdfi.dk>
2023-01-13 14:29:34 +0100

    update test

--- test/navigate.spec.js
+++ test/navigate.spec.js
@@ -9,4 +9,9 @@ test('Search for an address from start page', async ({ page }) => {
 
   // Expect main viewport to contain data
   await expect(page.locator('#viewport-main')).toContainText("Billede af området omkring koordinat 721760 øst,6178876 nord set fra nord.", { timeout: 10000 })
+  await expect(page.locator('#viewport-north')).toContainText("Billede af området omkring koordinat 721760 øst,6178876 nord set fra nord.", { timeout: 10000 })
+  await expect(page.locator('#viewport-nadir')).toContainText("Billede af området omkring koordinat 722482 øst,6178711 nord set fra top.", { timeout: 10000 })
+  await expect(page.locator('#viewport-east')).toContainText("Billede af området omkring koordinat 721879 øst,6178807 nord set fra øst.", { timeout: 10000 })
+  await expect(page.locator('#viewport-south')).toContainText("Billede af området omkring koordinat 722476 øst,6178944 nord set fra syd.", { timeout: 10000 })
+  await expect(page.locator('#viewport-west')).toContainText("Billede af området omkring koordinat 722392 øst,6178959 nord set fra vest.", { timeout: 10000 })
 })
