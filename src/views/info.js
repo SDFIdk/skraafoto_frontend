245e9a595714cfdde188d5e42b07b2494852c7cc
Frank Thomsen <frtho@sdfi.dk>
2023-01-16 17:23:03 +0100

    stop gap direct for HTTP pages

--- src/views/info.js
+++ src/views/info.js
@@ -1,6 +1,10 @@
 import { SkraaFotoHeader } from '../components/page-header.js'
 import { configuration } from '../modules/configuration.js'
 import { CookieAlert } from '../components/cookie-alert.js'
+import { redirect } from '../modules/redirect.js'
+
+// Redirect if page is HTTP
+redirect()
 
 customElements.define('skraafoto-header', SkraaFotoHeader)
 
