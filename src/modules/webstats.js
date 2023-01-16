f2d3554d6cb77ff9ef56ae90041b27dccc7d328b
Frank Thomsen <frtho@sdfi.dk>
2023-01-16 09:37:41 +0100

    clean up console logs

--- src/modules/webstats.js
+++ src/modules/webstats.js
@@ -3,7 +3,6 @@
 import { configuration } from "./configuration.js";
 
 function runWebStat() {
-  console.log('runs the webstats code')
   const snoop_script = document.createElement('script')
   snoop_script.src = configuration.SITEIMPROVE_SCRIPT
   snoop_script.async = true
