66d31686b280076a08519beac4e8ce966058ffdb
Rasmus Tønder <rtoender@hotmail.com>
2023-08-08 09:56:20 +0200

    fixed twinview bug

--- src/views/viewer.js
+++ src/views/viewer.js
@@ -91,7 +91,7 @@ document.addEventListener('gsearch:select', function(event) {
   const orientation = getParam('orientation') ? getParam('orientation') : 'north'
   if (orientation !== 'map') {
     queryItems(new_center, orientation, collection).then((response) => {
-      setParams({ center: new_center, item: response.features[0].id })
+      setParams({ center: new_center, item: response.features[0].id, item2: response.features[0].id })
     })
   } else {
     setParams({ center: new_center, item: null })
