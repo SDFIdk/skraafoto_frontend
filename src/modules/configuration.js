d3178071ed0a1991aa81ac175a8a943bc7a51aec
Frank Thomsen <frtho@sdfi.dk>
2023-08-25 12:33:18 +0200

    hack the stack

--- src/modules/configuration.js
+++ src/modules/configuration.js
@@ -3,10 +3,10 @@
 let configuration = {
 
   API_STAC_TOKEN: '', // STAC TOKEN can be aquired from https://dataforsyningen.dk/
-  API_STAC_BASEURL: "https://api.dataforsyningen.dk/rest/skraafoto_api/v1.0",
+  API_STAC_BASEURL: "https://test11.dataforsyningen.dk/api/rest/skraafoto_api/v1.0",
 
   API_DHM_WCS_BASEURL: "https://services.datafordeler.dk/DHMNedboer/dhm_wcs/1.0.0/WCS",
-  API_DHM_BASEURL: "https://services.datafordeler.dk/DHMTerraen/DHMKoter/1.0.0/GEOREST/HentKoter",
+  API_DHM_BASEURL: "https://test11.dataforsyningen.dk/DHMTerraen/DHMKoter/1.0.0/GEOREST/HentKoter",
   API_DHM_TOKENA: '', // DHM API service username can be created at https://datafordeler.dk/dataoversigt/danmarks-hoejdemodel-dhm/koter/
   API_DHM_TOKENB: '', // DHM API service password can be created at https://datafordeler.dk/dataoversigt/danmarks-hoejdemodel-dhm/koter/
 
