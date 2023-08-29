5c9296b613cca1f32b740e4fcba3906abaf782eb
Rasmus Tønder <rtoender@hotmail.com>
2023-08-29 11:19:00 +0200

    added confirm button to alertsplash

--- src/components/alert-splash.js
+++ src/components/alert-splash.js
@@ -3,11 +3,9 @@ import { configuration } from "../modules/configuration";
 export class AlertSplash extends HTMLElement {
   dialog
   styles = `
-  
-
-  
     dialog::backdrop {
       background-color: hsla(0, 0%, 0%, 0.6);
+      transition: backdrop-filter 0.2s;
     }
     dialog article {
       padding: var(--margin);
@@ -21,6 +19,16 @@ export class AlertSplash extends HTMLElement {
     dialog button {
       margin: 0;
     }
+    
+    .btn-confirm {
+      margin-top: 1rem;
+      transition: opacity 0.3s ease;
+    }
+    
+    .fade-out, .fade-out::backdrop {
+    opacity: 0;
+    transition: opacity 0.3s ease;
+  }
   `
 
   template = `
@@ -34,6 +42,7 @@ export class AlertSplash extends HTMLElement {
       <p>
       Vi oplever pt. driftsforstyrrelser i vores skråfoto frontend. Der arbejdes på at finde en løsning. Læs mere på: <a href="https://trello.com/c/HPZCGzwA">Trello</a>
       </p>
+      <button class="btn-confirm">Forstået</button>
     </article>
   `
 
@@ -57,6 +66,16 @@ export class AlertSplash extends HTMLElement {
   connectedCallback() {
     if (configuration.ENABLE_ALERT)
       this.dialog.showModal()
+
+    this.shadowRoot.querySelector('.btn-confirm').addEventListener('click', () => {
+      console.log('Button clicked');
+      this.dialog.classList.add('fade-out'); // Apply fade-out class
+      setTimeout(() => {
+        console.log('Closing dialog');
+        this.dialog.close();
+        this.dialog.classList.remove('fade-out'); // Remove the class after the transition completes
+      }, 300); // Adjust the timing to match the transition duration in milliseconds
+    });
   }
 }
 
