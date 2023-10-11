40acdcd49323e364a8fd29a4e12ee9b60a99d52a
Rasmus Tønder <49906703+rasmustoender@users.noreply.github.com>
2023-10-11 15:04:50 +0200

    Update viewport-mixin.js

--- src/modules/viewport-mixin.js
+++ src/modules/viewport-mixin.js
@@ -161,7 +161,6 @@ function getAdjustedNadirRotation(item) {
 
 /** Create a modified View object with min and max zoom levels */
 function createView(view_config) {
-  view_config.extent
   const view = new View(view_config)
   view.setMinZoom(configuration.MIN_ZOOM)
   view.setMaxZoom(configuration.MAX_ZOOM - configuration.OVERVIEW_ZOOM_DIFFERENCE)
