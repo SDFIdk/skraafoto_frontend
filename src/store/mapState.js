63500ec853a6ff1e639baab0a0ce74b5659c1dcf
Rasmus Tønder <rtoender@hotmail.com>
2023-10-17 14:21:59 +0200

    Revert "removed unused updateParcels() + updateCenter()"
    This reverts commit c9594a7fcba1b7588f33fab65e7c5b20c6703c88.

--- src/store/mapState.js
+++ src/store/mapState.js
@@ -24,12 +24,30 @@ const mapState = {
  * Actions for the map data
  */
 const mapActions = {
+  updateParcels: function(state, payload) {
+    state.parcels = payload
+    return state
+  },
   updateView: function(state, payload) {
     state.view = payload
     return state
   },
   updateMarker: function(state, payload) {
     state.marker = payload
+    return state
+  },
+  updateCenter: function(state, center) {
+    if (typeof center === 'string') {
+      // Assuming center is a comma separated string with up to 3 numbers
+      let cArray = center.split(',')
+      const coords = cArray.map((c) => Number(c))
+      state.view.kote = coords[2]
+      state.view.center = coords.slice(0,2)
+    } else {
+      // Assuming center is an array of up to 3 numbers
+      state.view.kote = center[2]
+      state.view.center = center.slice(0,2)
+    }
     return state
   },
   updateMapVisibility: function(state, isVisible) {
