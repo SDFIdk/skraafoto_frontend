8c5b37f9a15c9bc29901a4cac63e6ff012f03a83
Frank Thomsen <frtho@sdfi.dk>
2023-10-19 14:29:36 +0200

    change amount of workers for CI tests

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default {
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'list' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
