be2e1a8b1662f346815a7ac9d87db795e2c3c88b
Frank Thomsen <frtho@sdfi.dk>
2023-11-08 18:09:13 +0100

    edit comments

--- test/e2e/centering.spec.js
+++ test/e2e/centering.spec.js
@@ -25,16 +25,7 @@ test('Change position when clicking in the viewport', async ({ page }) => {
 })
 
 /*
-  - center viewport when URL center was given
-  - center viewport when no URL center is given
-  - center viewport when clicking the main viewport
-  - center viewport when using crosshair tool
-  - center viewport when selecting another direction
+
   - center viewport when selecting another collection
-
-
-test('center viewport when no URL center was given', async ({ page }) => {
-
-})
 
 */
