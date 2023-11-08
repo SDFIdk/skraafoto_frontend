07e89af2c6bfe3f652aa6da72342c3d13d3a6839
Frank Thomsen <frtho@sdfi.dk>
2023-11-08 15:56:39 +0100

    change test title

--- test/e2e/shortkeys.spec.js
+++ test/e2e/shortkeys.spec.js
@@ -11,7 +11,7 @@ test('Shift view from north to east using shortkeys', async ({ page }) => {
   await expect(page.locator('css=skraafoto-viewport')).toContainText('koordinat 722119 Ø, 6178801 N set fra øst.')
 })
 
-test('Shift to an earlier image using shortkeys', async ({ page }) => {
+test('Shift to earlier image using shortkeys', async ({ page }) => {
   await expect(page.locator('css=.sf-date-viewer')).toHaveValue('2023_84_40_2_0139_00061830')
   await page.keyboard.press('Shift+ArrowDown')
   await expect(page.locator('css=.sf-date-viewer')).toHaveValue('2023_84_40_2_0139_00061833')
