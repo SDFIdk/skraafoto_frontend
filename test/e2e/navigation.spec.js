8af7883f8688d97bfd03a47403e128518bb96135
Frank Thomsen <frtho@sdfi.dk>
2023-11-08 15:28:25 +0100

    add more test

--- test/e2e/navigation.spec.js
+++ test/e2e/navigation.spec.js
@@ -42,12 +42,10 @@ test('Change orientation to nadir', async ({ page }) => {
   await expect(page.locator('css=skraafoto-viewport')).toContainText('Billede af området omkring koordinat 534893 Ø, 6173611 N set fra top.')
 })
 
-/*
-
-test('Select map', async ({ page }) => {
-  // Go to page and check that it renders in the correct position
-  await page.goto('/?project=denmark&parcels=270554-15ax&address=Gartnervænget 3, 4160 Herlufmagle&ejendomsid=815465&x=674480.0132781528&y=6133107.9439362185&width=40&mode=4&token=e74200f9b819b0c96a656dce7e0d8850&year=2019', { waitUntil: 'networkidle' })
-  await expect(page.locator('css=#viewport-1')).toContainText('Billede af området omkring koordinat 674480 Ø, 6133108 N set fra nord.')
+test('Select map view', async ({ page }) => {
+  await page.goto('/?item=2023_83_29_2_0022_00002831', { waitUntil: 'networkidle' })
+  await page.getByTitle('Skift til kortvisning').click()
+  await page.waitForLoadState('networkidle')
+  await expect(page.locator('css=skraafoto-advanced-map')).toBeVisible()
+  await expect(page.locator('css=skraafoto-viewport')).toBeHidden()
 })
-
-*/
