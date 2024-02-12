bdf0546b8d92196b58872d45b4d3bb76efb176b5
Frank Thomsen <frank.thomsen@gmail.com>
2024-02-12 17:57:14 +0100

    copy files correctly

--- bin/copy-to-dist.js
+++ bin/copy-to-dist.js
@@ -4,6 +4,7 @@ async function copyFiles() {
   await cp('public/viewer.html', 'dist/viewer.html')
   await cp('public/redirected.html', 'dist/redirected.html')
   await cp('public/img', 'dist/img', {recursive: true})
+  await cp('public/was/index.html', 'dist/was/index.html')
 }
 
 export {
