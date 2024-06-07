3b4e23d28af2644b7d1d4ce9a80d9084f771a369
Frank R Thomsen <frtho@sdfi.dk>
2024-06-07 11:25:23 +0200

    insert questionnaire into first time visitor splash

--- src/components/first-time-visitor.js
+++ src/components/first-time-visitor.js
@@ -1,4 +1,5 @@
-import { getSharedStyles } from "../styles/shared-styles.js"
+import { configuration } from '../modules/configuration.js'
+import { getSharedStyles } from '../styles/shared-styles.js'
 
 /**
  * Web component that presents a greeting to first time visitors to the site
@@ -7,14 +8,25 @@ export class FirstTimeVisit extends HTMLElement {
 
   // Properties
   dialog
-  local_storage_key = 'skraafoto-splash-1'
+  local_storage_key = configuration.LOCAL_STORAGE_FIRST_TIME_VISITOR_KEY
   styles = `
+    dialog {
+      padding: 0 !important;
+      overflow: auto;
+    }
+    dialog[open] {
+      max-width: 100%;
+      width: 70rem;
+      margin: auto;
+      height: 100%;
+      max-height: 87.5rem;
+    }
     dialog::backdrop {
       background-color: hsla(0, 0%, 0%, 0.6);
       transition: backdrop-filter 0.2s;
     }
     dialog article {
-      padding: var(--margin);
+      padding: var(--space-lg) 0 0;
       display: flex;
       flex-flow: column nowrap;
       align-items: center;
@@ -26,7 +38,7 @@ export class FirstTimeVisit extends HTMLElement {
       margin: 0;
     }
     .btn-welcome-close {
-      margin-top: 4rem;
+      margin-top: var(--space-lg);
       transition: opacity 0.3s ease;
     }
     .fade-out, .fade-out::backdrop {
@@ -35,28 +47,29 @@ export class FirstTimeVisit extends HTMLElement {
     }
     ul li {
       text-align: left;
+    }
   `
   template = `
     ${ getSharedStyles() }
     <style>
       ${this.styles}
-      dialog {
-        padding: 0 !important;
-        overflow: visible;
-      }
     </style>
     
     <article>
-      <h2 class="h1">Velkommen til Skråfoto</h2>
-      <p>
-        Skråfoto giver dig mulighed for at se luftfotos taget fra forskellige retninger.
-        Søg efter en adresse eller et stednavn for at finde skråfotos i dit område.
-      </p>
-      <!---<skraafoto-address-search></skraafoto-address-search>--->
-      <h2>Vil du hjælpe os?</h2>
-      <p>Vi vil gerne vide noget mere om, hvem der bruger Skråfoto.</p>
-      <p>Hvis du har 2 minutter, kan du hjælpe os ved at<br><a target="_blank" href="https://www.survey-xact.dk/LinkCollector?key=TJ5EARGLU292">udfylde dette korte spørgeskema.</a></p>
-      <button class="btn-welcome-close">Forstået</button>
+      <div style="padding: var(--space-lg) var(--space-xl) var(--space-xl); text-align: center;">
+        <h2 class="h1" style="display: inline-block;">Velkommen til Skråfoto</h2>
+        <p>
+          Skråfoto giver dig mulighed for at se luftfotos taget fra forskellige retninger.
+          Søg efter en adresse eller et stednavn for at finde skråfotos i dit område.
+        </p>
+        <p>
+          <button class="btn-welcome-close">Forstået</button>
+        </p>
+        <h2 style="display: inline-block;">Vil du hjælpe os?</h2>
+        <p>Vi vil gerne vide noget mere om, hvem der bruger Skråfoto.</p>
+        <p>Hvis du har 2 minutter, kan du hjælpe os ved at<br>udfylde dette korte spørgeskema.</p>
+      </div>
+      <iframe style="height: 60rem; width: 100%; border: none;" src="https://www.survey-xact.dk/LinkCollector?key=TJ5EARGLU292"></iframe>
     </article>
   `
 
