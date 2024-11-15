46cc0b076eea20bfa860451cc416ea066a7d2df4
Frank R Thomsen <frtho@sdfi.dk>
2024-11-15 12:29:19 +0100

    tweak test

--- test/e2e/centering.spec.js
+++ test/e2e/centering.spec.js
@@ -11,10 +11,10 @@ test.beforeEach(async ({ page }) => {
 test('Change position when clicking in the viewport', async ({ page }) => {
   await page.goto('/?item=2023_84_40_2_0139_00061830', { waitUntil: 'networkidle' })
 
-  await expect(page.locator('css=skraafoto-viewport')).toContainText('koordinat 722119 Ø, 6178801 N set fra nord.')
+  await expect(page.locator('skraafoto-viewport')).toContainText('koordinat 722119 Ø, 6178801 N set fra nord.')
   await expect(page.getByTitle('Billede af området omkring koordinat 722119 Ø, 6178801 N set fra øst.')).toBeDefined()
 
-  const centerTool = await page.getByTitle('Vælg en ny position')
+  const centerTool = await page.locator('skraafoto-pin-tool button')
   if (centerTool) {
     await centerTool.click()
     await page.mouse.click(200,200)
