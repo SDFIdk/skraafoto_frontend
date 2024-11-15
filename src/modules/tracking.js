33956047287ea4c6600ef72250a2bba6e2221f0c
Frank R Thomsen <frank@sdfi.dk>
2024-11-15 11:37:38 +0100

    remove cookieinformation popup

--- src/modules/tracking.js
+++ src/modules/tracking.js
@@ -1,23 +1,10 @@
 import { configuration } from "../modules/configuration.js"
 
 export function setupAnalytics() {
-
   if (!configuration.ENABLE_WEB_STATISTICS) {
     return
   }
-
-  const cookieInformationScript = document.createElement('script')
   const siteImproveScript = document.createElement('script')
-
-  cookieInformationScript.id = 'CookieConsent'
-  cookieInformationScript.src = 'https://policy.app.cookieinformation.com/uc.js'
-  cookieInformationScript.dataset.culture = 'DA'
-
-  siteImproveScript.src = ''
-  siteImproveScript.setAttribute('data-consent-src', configuration.SITEIMPROVE_SCRIPT)
-  siteImproveScript.setAttribute('data-category-consent', 'cookie_cat_statistic')
-
-  document.body.append(cookieInformationScript)
+  siteImproveScript.src = configuration.SITEIMPROVE_SCRIPT
   document.body.append(siteImproveScript)
-  
 }
