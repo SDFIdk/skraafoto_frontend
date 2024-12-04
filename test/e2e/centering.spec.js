95fd65b2d06bda85a6cf51c976cee1810cbd3895
Frank R Thomsen <frank@sdfi.dk>
2024-12-04 12:19:25 +0100

    tweak tests

--- test/e2e/centering.spec.js
+++ test/e2e/centering.spec.js
@@ -10,7 +10,6 @@ test.beforeEach(async ({ page }) => {
 
 test('Change position when clicking in the viewport 1', async ({ page }) => {
   await page.goto('/?item=2023_84_40_2_0139_00061830', { waitUntil: 'networkidle' })
-
   await expect(page.locator('skraafoto-viewport')).toContainText('koordinat 722119 Ø, 6178801 N set fra nord.')
   await expect(page.getByTitle('Billede af området omkring koordinat 722119 Ø, 6178801 N set fra øst.')).toBeDefined()
 })
--- test/e2e/collection.spec.js
+++ test/e2e/collection.spec.js
@@ -8,10 +8,13 @@ test.beforeEach(async ({ page }) => {
   }, configuration)
 })
 
-test('Change images when selecting another collection', async ({ page }) => {
-  await page.goto('/?item=2023_84_40_2_0139_00061830', { waitUntil: 'networkidle' })
-
+test('Change images when selecting another collection 1', async ({ page }) => {
+  await page.goto('/?item=2023_84_40_2_0139_00061830')
   await expect(page.locator('.sf-date-selector')).toHaveValue('2023_84_40_2_0139_00061830')
+})
+
+test.slow('Change images when selecting another collection 2', async ({ page }) => {
+  await page.goto('/?item=2023_84_40_2_0139_00061830', { waitUntil: 'networkidle' })
   await page.getByTitle('Vælg en årgang').selectOption('2017')
   await expect(page.locator('.sf-date-selector')).toHaveValue('2017_84_40_2_0041_00001389')
   await expect(page.url()).toContain('item=2017_84_40_2_0041_00001389')
