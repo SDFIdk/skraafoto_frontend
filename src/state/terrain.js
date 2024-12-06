81ae88a79b654660bf1d02514529636406a3c703
Frank R Thomsen <frank@sdfi.dk>
2024-12-06 14:49:04 +0100

    update terrain when necessary

--- src/state/terrain.js
+++ src/state/terrain.js
@@ -1,12 +1,13 @@
-import { get, consumeGeoTIFF } from '@dataforsyningen/saul'
+import { get, consumeGeoTIFF, getTerrainByBbox } from '@dataforsyningen/saul'
 import DKGeoTiff from '@dataforsyningen/saul/assets/dk-terrain.tiff'
+import { configuration } from '../modules/configuration.js'
 
 /** 
  * Iterates over an array of image items 
  * and returns a total bounding box for all the image items
  */
 export function getTotalBbox(imageItems) {
-  const bbox = imageItems
+  const bbox = imageItems.nadir.bbox
   for (const [key, item] of Object.entries(imageItems)) {
     if (item === null) {
       continue
@@ -28,32 +29,54 @@ export function getTotalBbox(imageItems) {
   return bbox
 }
 
-export function getTerrain(imageItems) {
+export function getTerrain(imageItems, currentTerrain) {
   return new Promise((resolve, reject) => {
     if (imageItems.nadir && imageItems.north && imageItems.south && imageItems.east && imageItems.west) {
+
       const bbox = getTotalBbox(imageItems)
-      return get(DKGeoTiff, {cache: 'force-cache'}, false)
-      .then((response) => {
-        return response.arrayBuffer()
-      })
-      .then(async (arrayBuffer) => {
-        consumeGeoTIFF(arrayBuffer).then(gtiff => {
-          const terrainData = {
-            bbox: bbox,
-            data: gtiff
-          }
-          resolve(terrainData)
-        })
-        
+      
+      // If bbox is the same, the terrain data will probably be the same. No need to refetch.
+      if (currentTerrain.bbox === bbox) {
+        console.log('are they the same?')
+        resolve(currentTerrain)
+      }
+      
+      // First, try to get a GeoTiff elevation model for your particular bounding box from Datafordeler
+      getTerrainByBbox(bbox, configuration).then(gtiff => {
+        const terrainData = {
+          bbox: bbox,
+          data: gtiff
+        }
+        resolve(terrainData)
       })
       .catch(error => {
-        reject(error)
+        // If Datafordeler fails, fallback to using local GeoTiff elevation model
+        get(DKGeoTiff, {cache: 'force-cache'}, false)
+        .then((response) => {
+          return response.arrayBuffer()
+        })
+        .then(async (arrayBuffer) => {
+          consumeGeoTIFF(arrayBuffer).then(gtiff => {
+            const terrainData = {
+              bbox: [430000,6040000,900000,6413000],
+              data: gtiff
+            }
+            resolve(terrainData)
+          })
+          
+        })
+        .catch(error => {
+          reject(error)
+        })
       })
+
     } else {
+
       // If items aren't loaded yet, wait a while and retry
       setTimeout(() => {
         return getTerrain(imageItems)
       }, 500)
+
     }
   })
 }
