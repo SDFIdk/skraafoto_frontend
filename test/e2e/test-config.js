5971d515c66670311604a5462f821e1e25fff44a
Johan Lund Neergaard <johanlundn@gmail.com>
2025-07-01 13:03:01 +0200

    Update test-config.js

--- test/e2e/test-config.js
+++ test/e2e/test-config.js
@@ -1,5 +1,4 @@
-// Note that this has to be manually updated to match the value in 'src/modules/configuration'.
-// We can't import it because it fails due to no global `config` variable existing.
+// Note that this has to be manually updated to match the value in the configurations.
 export const configuration = {
-  LOCAL_STORAGE_FIRST_TIME_VISITOR_KEY: 'skraafoto-first-time-visit'
+  LOCAL_STORAGE_FIRST_TIME_VISITOR_KEY: 'skraafoto-release-december-2024'
 }
