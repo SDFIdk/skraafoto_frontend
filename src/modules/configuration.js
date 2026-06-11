664c9d85ceacb1a40ed2971acd3416a027c488c9
Mac Wendelboe <macwe@kds.dk>
2026-06-11 15:45:09 +0200

    feat!: DHM WCS endpoint with API key
    Switch to Datafordeler endpoint that accepts API key instead of user
    name and password.

--- src/modules/configuration.js
+++ src/modules/configuration.js
@@ -20,7 +20,7 @@ let configuration = {
   API_STAC_TOKEN: '', // STAC TOKEN can be aquired from https://dataforsyningen.dk/
   API_STAC_BASEURL: api_stac_default_prod,
 
-  API_DHM_WCS_BASEURL: "https://services.datafordeler.dk/DHMNedboer/dhm_wcs/1.0.0/WCS",
+  API_DHM_WCS_BASEURL: "https://wcs.datafordeler.dk/DHMNedboer/dhm_wcs/1.0.0/WCS",
   API_DHM_BASEURL: "https://services.datafordeler.dk/DHMTerraen/DHMKoter/1.0.0/GEOREST/HentKoter",
   API_DHM_TOKENA: '', // DHM API service username can be created at https://datafordeler.dk/dataoversigt/danmarks-hoejdemodel-dhm/koter/
   API_DHM_TOKENB: '', // DHM API service password can be created at https://datafordeler.dk/dataoversigt/danmarks-hoejdemodel-dhm/koter/
